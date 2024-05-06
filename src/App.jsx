20ae1cab154175e27c0ba557747bca8b46b69d83
shailesh <shaileshsingh121122@gmail.com>
2024-05-07 00:14:20 +0530

    redux setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
 
-import './App.css'
+import { useEffect, useState } from 'react'
+import authService from './appwrite/auth'
+import { useDispatch } from 'react-redux'
+import { login, logout } from './store/authSlice'
+import { Outlet } from 'react-router-dom'
+import {Header, Footer} from './components'
+
 
 function App() {
-  
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>My Bolg app with appwrite</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({userData}))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+    
+  }, [])
+
+  return !loading ? (
+    <div className=' min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        TODO: <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
